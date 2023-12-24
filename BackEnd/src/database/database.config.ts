8d206ab4cfa9b2b1af601fcfaaddf7370da725c6
javier <nhat1234559@gmail.com>
2023-12-25 02:22:01 +0700

    refactor!: update client and server

--- BackEnd/src/database/database.config.ts
+++
@@ -1,7 +0,0 @@
-// Nếu ae setup mongodb COMPASS thì copy link bỏ vào đây để test nha
-// SEVER là link connect thì mongodb luôn nhé
-
-export enum MONGODB_CONFIG {
-  COMPASS = '',
-  DATABASE_CONNECTION = `mongodb+srv://vocab:dt01626746032@cluster0.2ktatdl.mongodb.net/?retryWrites=true&w=majority`,
-}
